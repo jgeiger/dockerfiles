77348d9c1b4cf3f280f75042b4b8c9629f30c87b
jgeiger <joey.geiger@irco.com>
2016-02-23 22:44:15 -0700

    Add rails-integration to include xvfb, vnc, chrome, chromedriver

---
+++ rails/integration/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+sudo Xvfb $DISPLAY -ac -screen 0 $GEOMETRY 2>/dev/null &
+sudo fluxbox -display $DISPLAY 2>/dev/null &
+sudo x11vnc -noxdamage -forever -usepw -shared -rfbport 5900 -display $DISPLAY 2>/dev/null &
+
+echo "Installing gems as needed..."
+sudo chown -R devops:devops /usr/local/bundle
+sudo chmod go-w /usr/local/bundle
+bundle install --quiet --path /usr/local/bundle --jobs 20 --retry 5
+
+exec "$@"
