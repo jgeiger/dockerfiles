8fd74601089ec007dbf8bbb10dc0ef1f7790836f
jgeiger <joey.geiger@irco.com>
2016-03-26 20:26:26 -0600

    Remove xvfb and x11vnc, use tightvncserver
    - also increase the default size of the screen

--- rails/integration/docker-entrypoint.sh
+++ rails/integration/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
-sudo Xvfb $DISPLAY -ac -screen 0 $GEOMETRY 2>/dev/null &
-sudo fluxbox -display $DISPLAY 2>/dev/null &
-sudo x11vnc -noxdamage -forever -usepw -shared -rfbport 5900 -display $DISPLAY 2>/dev/null &
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT"
+
+tightvncserver $VNC_DISPLAY -geometry $GEOMETRY
+fluxbox -display $DISPLAY 2>/dev/null &
 
 echo "Installing gems as needed..."
 sudo chown -R $USERNAME:$USERNAME /usr/local/bundle
